d718dbfef424d2f9a3eecd2a481c8dfe67136dc6
Andreas Eriksson <andreas.eriksson@netclean.com>
2015-11-19 16:06:58 +0100

    Added SetErrorCode and _set_abort_behavior to prevent crash dialogs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#define WIN32_LEAN_AND_MEAN
+#include <stdlib.h>
+#include <windows.h>
 #include "Program.h"
 #include <iostream>
 #include <string>
@@ -5,6 +8,10 @@
 
 int main(int argc, char** argv)
 {
+	SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
+	// Suppress the abort message
+	_set_abort_behavior(0, _WRITE_ABORT_MSG);
+
 	if(argc != 2){
 		std::cout << "usage: " << argv[0] << " [shmname]" << std::endl;
 		return 1;
